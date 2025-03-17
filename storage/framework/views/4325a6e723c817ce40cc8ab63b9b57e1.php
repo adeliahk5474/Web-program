6e67667f047dcd8bb20b660768c50208aaf6f255
adeliahk5474 <144311220+adeliahk5474@users.noreply.github.com>
2025-03-17 07:23:14 +0700

    Add files via upload

---
+++ storage/framework/views/4325a6e723c817ce40cc8ab63b9b57e1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About Us</title>
+</head>
+
+<body>
+    <h1>About Us</h1>
+    <p>This is the about page.</p>
+</body>
+
+</html>
+<?php /**PATH C:\xampp\htdocs\pemrogramweb\resources\views/about.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4c3358e976ad7a3841d5a2109e7053d5.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Picture Us</title>
+</head>
+
+<body>
+    <h1>Picture Us</h1>
+    <p>This is the about page.</p>
+</body>
+
+</html>
+<?php /**PATH C:\xampp\htdocs\pemrogramweb\resources\views/picture.blade.php ENDPATH**/ ?>
